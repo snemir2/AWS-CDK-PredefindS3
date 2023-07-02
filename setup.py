f0f346fbf7de57db809ce774565c8276591860dd
Sergey Nemirovsky <sergey@a2-ai.com>
2023-07-02 13:39:27 -0400

    test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from PredefinedS3 import __version__
 setup(
     name="PredefinedS3",
-    version= __version__,
-    packages=find_packages(),
+    description="Commonly used S3 bucket configurations",
+    packages=['PredefinedS3'],
   
 )
