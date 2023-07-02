c2eff8e4073d0543b729b850c5dc65833c76d28f
Sergey Nemirovsky <sergey@a2-ai.com>
2023-07-02 13:24:09 -0400

    .

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from PredefinedS3 import __version__
 setup(
     name="PredefinedS3",
     version= __version__,
+    package_dir={"": "."},
     packages=find_packages(),
-    py_modules=['PredefinedS3'],
+  
 )
