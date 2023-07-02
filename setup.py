64103df1c98f2e34331039e39b30e671eae8a997
Sergey Nemirovsky <sergey@a2-ai.com>
2023-07-02 13:41:46 -0400

    feat: hardcodding version

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
-from PredefinedS3 import __version__
+
 setup(
     name="PredefinedS3",
     description="Commonly used S3 bucket configurations",
     packages=['PredefinedS3'],
-  
+    version="0.0.1"
 )
